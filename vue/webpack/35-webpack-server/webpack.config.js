add8cdcbdc1f8f9fcb485ef82227d16339441ce5
wangerfei <83889573@qq.com>
2022-03-15 16:09:28 +0800

    修复因为index.html中手动引入js导致vue样式加载两次的问题

--- vue/webpack/35-webpack-server/webpack.config.js
+++ vue/webpack/35-webpack-server/webpack.config.js
@@ -17,13 +17,13 @@ module.exports = {
   module:{
     rules:[
       {
+        test: /\.css$/i,
+        use:["style-loader","css-loader"]      
+      },
+      {
         //匹配正则表达式的文件，执行use中的loader
         test: /\.vue$/i,
         use:["vue-loader"],
-      },
-      {
-        test: /\.css$/i,
-        use:["style-loader","css-loader"]      
       }
     ]
   },
@@ -32,9 +32,9 @@ module.exports = {
     new VueLoaderPlugin(),
     new webpack.DefinePlugin({
       //vue3中新增了组合式api,原本vue3之前使用的选项式api，下边这参数就是选项式api的开关
-      // __VUE_OPTIONS_API__: true,
+      __VUE_OPTIONS_API__: true,
       //在生产环境中是否允许使用浏览器插件vue-devtools
-      // __VUE_PROD_DEVTOOLS__: false,
+      __VUE_PROD_DEVTOOLS__: false,
     }),
     new htmlWebpackPlugin({
       template:"index.html"
@@ -43,7 +43,7 @@ module.exports = {
   resolve: {
     alias: {
       //指定vue版本
-      vue: 'vue/dist/vue.esm-bundler.js',
+      vue$: 'vue/dist/vue.esm-bundler.js',
     },
   },
   devServer: {
